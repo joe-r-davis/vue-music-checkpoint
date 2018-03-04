32f92d69a7291a2d11bf5b729d27f235233b785f
Joe Davis <joecrush@msn.com>
2018-03-03 19:40:18 -0700

    updated songs routes to target checkpoint reqs

--- server/server-assets/routes/songs.js
+++ server/server-assets/routes/songs.js
@@ -5,7 +5,7 @@ var Playlists = require("../models/playlist")
 //get all songs
 router.get('/music/songs', (req, res, next) => {
     Songs.find(req.query) //{} empty returns everything
-        .then(songs =>{
+        .then(songs => {
             return res.send(songs);
         })
         .catch(next);
@@ -14,70 +14,41 @@ router.get('/music/songs', (req, res, next) => {
 //get songs by id
 router.get('/music/songs/:sid', (req, res, next) => {
     Songs.findById(req.params.sid)
-        .then(songs =>{
+        .then(songs => {
             return res.send(songs);
         })
         .catch(next);
 })
 
-//get playlist
-router.get('/music/playlists', (req, res, next) => {
-    Playlists.find()
-        .then(playlists =>{
-            return res.send(playlists);
-        })
-        .catch(next);
-})
-
-
-//get playlsit by id
-router.get('/music/playlists/:pid/songs', (req, res, next) => {
-    var out = {}
-    Playlists.findById(req.params.pid)
-        .then(playlist => {
-            out.playlist = playlist
-            Songs.find({ playlistId: req.params.pid })
-                .then(songs => {
-                    out.songs = songs
-                    res.send(out)
-                })
-                .catch(next);
-        })
-        .catch(next);
-})
-
-
 //add song
-router.post('/music/songs', (req,res, next) => {
+router.post('/music/songs', (req, res, next) => {
     Songs.create(req.body)
-    .then(song => {
-        var response = {
-            data: song,
-            message: 'saved song to database!'
-        }
-        return res.send(response)
-    })
-    .catch(next);
+        .then(song => {
+            var response = {
+                data: song,
+                message: 'saved song to database!'
+            }
+            return res.send(response)
+        })
+        .catch(next);
 })
 
+//add song to playlist with playlist id (might have to be a stretch goal)
+// router.post('/music/playlists/:pid/songs', (req, res, next) => {
+//     req.body.playlistId = req.params.pid
+//     Songs.create(req.body)
+//         .then(song => {
+//             var response = {
+//                 data: song,
+//                 message: 'Successfully added song to playlist!'
+//             }
+//             res.send(response)
+//         })
+//         .catch(next);
+// })
 
-//add playlist
-router.post('/music/playlists', (req,res, next) => {
-    Playlists.create(req.body)
-    .then(playlist => {
-        var response = {
-            data: playlist,
-            message: 'Added playlist!'
-        }
-        return res.send(response)
-    })
-    .catch(next);
-})
-
-
-//add song to playlist
-router.post('/music/playlists/:pid/songs', (req, res, next) => {
-    req.body.playlistId = req.params.pid
+//add song to playlist - Need to make one playlist work first to meet reqs
+router.post('/music/playlists', (req, res, next) => {
     Songs.create(req.body)
         .then(song => {
             var response = {
@@ -87,48 +58,47 @@ router.post('/music/playlists/:pid/songs', (req, res, next) => {
             res.send(response)
         })
         .catch(next);
-    })
-
+})
 
 //update song on playlist
-router.put('/music/playlists/:pid/songs/:id', (req,res, next) => {
+router.put('/music/playlists/:id/songs/:id', (req, res, next) => {
     Songs.findByIdAndUpdate(req.params.id, req.body)
-    .then(song => {
-        var response = {
-            data: song,
-            message: 'Updated song on playlist!'
-        }
-        return res.send(response)
-    })
-    .catch(next);
+        .then(song => {
+            var response = {
+                data: song,
+                message: 'Updated song on playlist!'
+            }
+            return res.send(response)
+        })
+        .catch(next);
 })
 
 
-//update song
-router.put('/music/songs/:id', (req,res, next) => {
+//update song- this for up and down votes
+router.put('/music/songs/:id', (req, res, next) => {
     Songs.findByIdAndUpdate(req.params.id, req.body)
-    .then(song => {
-        var response = {
-            data: song,
-            message: 'Updated song on playlist!'
-        }
-        return res.send(response)
-    })
-    .catch(next);
+        .then(song => {
+            var response = {
+                data: song,
+                message: 'Updated song on playlist!'
+            }
+            return res.send(response)
+        })
+        .catch(next);
 })
 
 
 //delete song
-router.delete('/music/songs/:id', (req,res, next) => {
+router.delete('/music/songs/:id', (req, res, next) => {
     Songs.findByIdAndRemove(req.params.id)
-    .then(song => {
-        var response = {
-            data: song,
-            message: 'Deleted song from playlist!'
-        }
-        return res.send(response)
-    })
-    .catch(next);
+        .then(song => {
+            var response = {
+                data: song,
+                message: 'Deleted song from playlist!'
+            }
+            return res.send(response)
+        })
+        .catch(next);
 })
 
 module.exports = { router };

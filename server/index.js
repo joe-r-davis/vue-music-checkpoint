10860486e9ee2e16dd5031d53d1ec91840c8762a
Joe Davis <joecrush@msn.com>
2018-05-10 12:31:45 -0600

    Ready to deploy

--- server/index.js
+++ server/index.js
@@ -2,12 +2,12 @@ let express = require("express"),
   bp = require("body-parser"),
   cors = require("cors"),
   server = express(),
-  port = 3000;
+  port = process.env.PORT || 3000;
   var songsRoutes = require('./server-assets/routes/songs');
 
 require("./server-assets/db/mlab-config");
 
-var whitelist = ['http://localhost:8080'];
+var whitelist = ['http://localhost:8080', 'https://itunes-playlist.herokuapp.com/'];
 var corsOptions = {
 	origin: function (origin, callback) {
 		var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
@@ -20,6 +20,7 @@ var corsOptions = {
 server.use(cors(corsOptions));
 server.use(bp.json());
 server.use(bp.urlencoded({ extended: true }));
+server.use(express.static(__dirname + "/../public/dist"))
 server.use(songsRoutes.router);
 //Your routes here
 
